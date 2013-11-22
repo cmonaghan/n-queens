66a863af1dd3877718b300332b133103f8d0b2b8
Christian Monaghan <christian.monaghan@gmail.com>
2013-11-21 20:01:32 -0800

    Add partial solution to countNQueensSolution

--- src/solvers.js
+++ src/solvers.js
@@ -59,7 +59,7 @@ window.countNRooksSolutions = function(n){
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n queens placed such that none of them can attack each other
 window.findNQueensSolution = function(n){
-  var solution = undefined; //fixme
+  var solution = undefined;
 
   console.log('Single solution for ' + n + ' queens:', JSON.stringify(solution));
   return solution;
@@ -68,8 +68,70 @@ window.findNQueensSolution = function(n){
 
 // return the number of nxn chessboards that exist, with n queens placed such that none of them can attack each other
 window.countNQueensSolutions = function(n){
-  var solutionCount = undefined; //fixme
+  var counter = 0;
+  var possiblePositions = _.range(0, n);
+  var majorDiagonalPositions = [];
+  var minorDiagonalPositions = [];
 
-  console.log('Number of solutions for ' + n + ' queens:', solutionCount);
-  return solutionCount;
+  // function to remove diagonals from copy array
+  var removeDiagonals = function (possibleRowPositions, diagonal) {
+    for (var i = 0; i < diagonal.length; i++) {
+      if (possibleRowPositions.indexOf(diagonal[i]) !== -1) {
+        var idx = possibleRowPositions.indexOf(diagonal[i]);
+        possibleRowPositions.splice(idx, 1);
+      }
+    }
+  };
+
+  var iterator = function(possiblePositions, majorDiagonalPositions, minorDiagonalPositions) {
+
+    if (possiblePositions.length === 0) {
+      counter++;
+      return;
+    }
+
+    // copy possiblePositions
+    var possibleRowPositions = possiblePositions.slice();
+
+    // remove diagonals from copy
+    removeDiagonals(possibleRowPositions, majorDiagonalPositions);
+    removeDiagonals(possibleRowPositions, minorDiagonalPositions);
+
+    // for loop over each index on row (using possibleRowPositions)
+    for (var colIdx = 0; colIdx < possibleRowPositions.length; colIdx++) {
+
+      // Loop over rows
+      for (var rowIdx = 0; rowIdx < n; rowIdx++) {
+
+        // push chosen position to diagonals
+        majorDiagonalPositions.push(colIdx);
+        minorDiagonalPositions.push(colIdx);
+
+        // remove chosen value from copy
+        possibleRowPositions.splice(colIdx, 1);
+
+        // remove same value from possiblePositions
+        possiblePositions.splice(colIdx, 1);
+
+        // call iterator(possiblePositions, M, m)
+        iterator(possiblePositions, majorDiagonalPositions, minorDiagonalPositions)
+
+        // M++
+        for (var i = 0; i < majorDiagonalPositions.length; i++) {
+          majorDiagonalPositions[i] += 1;
+        }
+
+        // m--
+        for (var i = 0; i < minorDiagonalPositions.length; i++) {
+          minorDiagonalPositions[i] -= 1;
+        }
+      }
+    }
+  };
+
+  // call the iterator
+  iterator(possiblePositions, majorDiagonalPositions, minorDiagonalPositions);
+
+  console.log('Number of solutions for ' + n + ' queens:', counter);
+  return counter;
 };
